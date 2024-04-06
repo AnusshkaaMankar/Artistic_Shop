830bfe811c2ae6fa575d6b8942e4a01064d48fc1
codingwithkrish <87133705+codingwithkrish@users.noreply.github.com>
2024-04-06 17:11:18 +0530

    integrated firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,37 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:my_prj/Const/constants.dart';
 import 'package:my_prj/views/splash_screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner:false ,
+      debugShowCheckedModeBanner: false,
       title: 'Artistic Shop',
       theme: ThemeData(
-        scaffoldBackgroundColor: const Color.fromARGB(0, 146, 55, 55),
-        appBarTheme: const AppBarTheme(/*
+          scaffoldBackgroundColor: const Color.fromARGB(0, 146, 55, 55),
+          appBarTheme: const AppBarTheme(
+              /*
           iconTheme: IconThemeData(
             color: lightGrey
           ),*/
-            backgroundColor: Colors.transparent),
-        fontFamily: regular
-      ),
-      home:const SplashScreen(),
+              backgroundColor: Colors.transparent),
+          fontFamily: regular),
+      home: const SplashScreen(),
     );
   }
 }
